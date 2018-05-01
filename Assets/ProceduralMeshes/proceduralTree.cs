26d1e53f9ce77d7269a63c79e0b770c898360ed3
U-DESKTOP-NF6IRCC\luc <luc@gluc.ch>
2018-05-01 02:16:42 +0200

    better procedural tree + textured

--- Assets/ProceduralMeshes/proceduralTree.cs
+++ Assets/ProceduralMeshes/proceduralTree.cs
@@ -2,23 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public struct meshdata
-{
-    public Vector3[] vertices;
-    public int[] triangles;
-}
-
 [RequireComponent(typeof(MeshFilter))]
 public class proceduralTree : MonoBehaviour {
 
 	Mesh mesh;
+    Material material;
+
     Random rnd;
 	Vector3[] vertices;
+    Vector2[] uv;
     int nbVertices;
 	int[] triangles;
     int nbTriangles;
 
-	void Awake ()
+
+    void Awake ()
 	{
 		mesh = GetComponent<MeshFilter>().mesh;
 	}
@@ -28,10 +26,8 @@ public class proceduralTree : MonoBehaviour {
         CreateMesh();
     }
 
-    Vector3[] GenCircle (float r, float h, Vector3 n, Vector3 pos, int nbVertices)
+    int GenCircle (Vector3[] vertices, Vector2[] uv, int idx, float r, Vector3 n, Vector3 pos, int nbVert, float up)
     {
-        Vector3[] vertices;
-
         n.Normalize();
         Vector3 front = new Vector3(0, 0, 1);
         Vector3 right = Vector3.Cross(front, n).normalized;
@@ -44,190 +40,144 @@ public class proceduralTree : MonoBehaviour {
         mat = mat.transpose;
         mat = Matrix4x4.Translate(pos) * mat;
 
-        vertices = new Vector3[nbVertices];
+        float dv = 2 * Mathf.PI / nbVert;
 
-        float dv = 2 * Mathf.PI / nbVertices;
-
-        for (int i = 0; i < nbVertices; i++)
+        for (int i = 0; i < nbVert; i++)
         {
             Vector4 v = new Vector4(r * Mathf.Cos(dv * i), 0.0f, r * Mathf.Sin(dv * i), 1);
             v = mat * v;
-            vertices[i] = new Vector3(v.x, v.y, v.z);
+            vertices[idx + i] = new Vector3(v.x, v.y, v.z);
+            uv[idx + i] = new Vector2(4.0f * i / nbVert, up);
         }
-
-        return vertices;
+        return idx + nbVert;
     }
     /**
      * start1Offset : index to the first vertex of the first circle
      * start2Offset : index to the first vertex of the second circle
      */
-    int[] ConnectCircles (int offset1, int offset2, int length)
+    int ConnectCircles (int idx, int offset1, int offset2, int length)
     {
-        int[] triangles = new int[length * 3 * 2];
+        //int[] triangles = new int[length * 3 * 2];
         int i, j;
         for (i = 0, j = 0; i < 6 * (length - 1); i += 6, j++)
         {
-            triangles[i] = offset2 + j;
-            triangles[i + 1] = offset1 + j + 1;
-            triangles[i + 2] = offset1 + j;
+            triangles[idx + i] = offset2 + j;
+            triangles[idx + i + 1] = offset1 + j + 1;
+            triangles[idx + i + 2] = offset1 + j;
 
-            triangles[i + 3] = offset2 + j;
-            triangles[i + 4] = offset2 + j + 1;
-            triangles[i + 5] = offset1 + j + 1;
+            triangles[idx + i + 3] = offset2 + j;
+            triangles[idx + i + 4] = offset2 + j + 1;
+            triangles[idx + i + 5] = offset1 + j + 1;
         }
 
-        triangles[i] = offset2 + length - 1;
-        triangles[i + 1] = offset1;
-        triangles[i + 2] = offset1 + length - 1;
+        triangles[idx + i] = offset2 + length - 1;
+        triangles[idx + i + 1] = offset1;
+        triangles[idx + i + 2] = offset1 + length - 1;
 
-        triangles[i + 3] = offset2 + length - 1;
-        triangles[i + 4] = offset2;
-        triangles[i + 5] = offset1;
+        triangles[idx + i + 3] = offset2 + length - 1;
+        triangles[idx + i + 4] = offset2;
+        triangles[idx + i + 5] = offset1;
 
-        return triangles;
+        return idx + length * 6;
     }
 
-    meshdata GenCylinder (float r, float h, Vector3 n, Vector3 pos, int nbVertices)
+    Vector3 PerpVector (Vector3 dir)
     {
-        Vector3[] vertices;
-        int[] triangles;
-
-        n.Normalize();
-        Vector3 front = new Vector3(0, 0, 1);
-        Vector3 right = Vector3.Cross(front, n).normalized;
-        front = Vector3.Cross(right, n);
-
-        Matrix4x4 mat = new Matrix4x4(new Vector4(right.x, right.y, right.z, 0), 
-                                      new Vector4(n.x, n.y, n.z, 0.0f),
-                                      new Vector4(front.x, front.y, front.z, 0),
-                                      new Vector4(0, 0, 0, 1));
-
-        mat = mat.transpose;
-        mat = Matrix4x4.Translate (pos) * mat;
-
-
-        vertices = new Vector3[nbVertices];
-        triangles = new int[nbVertices * 3];
-
-        float dv = 4 * Mathf.PI / nbVertices;
-        int i, j;
-
-        for (i = 0; i < nbVertices/2; i++)
-        {
-            Vector4 v = new Vector4 (r * Mathf.Cos(dv * i), 0.0f, r * Mathf.Sin(dv * i), 1);
-            v = mat * v;
-            vertices[i] = new Vector3(v.x, v.y, v.z);
-        }
-        for (i = nbVertices / 2; i < nbVertices; i++)
-        {
-            Vector4 v = new Vector4 (r * Mathf.Cos(dv * i), h, r * Mathf.Sin(dv * i), 1);
-            v = mat * v;
-            vertices[i] = new Vector3(v.x, v.y, v.z);
-        }
-
-        for (i = 0, j = 0; i < 3 * (nbVertices - 2); i += 6, j++)
-        {
-            triangles[i+2] = j;
-            triangles[i + 1] = j + 1;
-            triangles[i] = nbVertices / 2 + j;
-
-            triangles[i + 5] = j + 1;
-            triangles[i + 4] = nbVertices / 2 + j + 1;
-            triangles[i + 3] = nbVertices / 2 + j;
-        }
-        triangles[i + 2] = nbVertices / 2 - 1;
-        triangles[i + 1] = 0;
-        triangles[i] = nbVertices - 1;
-
-        triangles[i + 5] = 0;
-        triangles[i + 4] = nbVertices / 2;
-        triangles[i + 3] = nbVertices - 1;
-
-        meshdata m;
-        m.vertices = vertices;
-        m.triangles = triangles;
-        return m;
+        Vector3 v = new Vector3(Random.Range(-1.0f, 1.0f),
+                                Random.Range(-1.0f, 1.0f),
+                                Random.Range(-1.0f, 1.0f));
+ 
+        return Vector3.Normalize (Vector3.Cross(v, dir));
     }
 
-    int GenTree (int baseOffet, float r, Vector3 n, Vector3 pos, int nbVertices)
+    Vector3 GenNormal (Vector3 dir, float alpha, float beta)
     {
-        float theta = Random.Range(0.0f, 0.78f);
+        Vector3 up = new Vector3(0, 1, 0);
+        Vector3 t = PerpVector (dir);
+        t = alpha * dir + (1.0f - alpha) * t;
+        return beta * up + (1.0f - beta) * t;
+    }
 
-        if (r < 0.1)
+    /*
+     * r is radius
+     * l is length 
+     * alpha influences tendencies to goes alongside branch dir
+     * beta influences tendencies to goes upward
+     * v = v coordinate of uv
+    */
+    int GenTree (int baseOffet, float r, float l, float alpha, float beta, Vector3 n, Vector3 pos, int vert, float v)
+    {
+        float rnd = Random.Range(0.0f, 1.0f);
+
+        if (r < 0.2f)
         {
-            return baseOffet;
+            return baseOffet + vert;
         }
-        else if (theta > 0.3f)
+        else if (rnd < 0.25f)
         {
-            Vector3 new_n = n;
-            Vector3 new_pos = pos;
+            Vector3 n1 = GenNormal(n, alpha, beta);
+            Vector3 n2 = GenNormal(n, alpha, beta);
 
-            Vector3[] circle = GenCircle(r, r*5, new_n, new_pos, nbVertices);
-            circle.CopyTo(vertices, nbVertices);
-            nbVertices += nbVertices;
+            //To be sure that they don't go both to the same direction
+            while (Vector3.Dot(n1, n2) > 0.92)
+            {
+                n2 = GenNormal(n, alpha, beta);
+            }
 
-            int[] index = ConnectCircles(baseOffet, baseOffet + nbVertices, nbVertices);
-            index.CopyTo(triangles, nbTriangles);
-            nbTriangles += index.Length;
+            float d1 = l * Random.Range(0.8f, 1.2f);
+            float d2 = l * Random.Range(0.8f, 1.2f);
+            Vector3 pos1 = pos + n1 * d1;
+            Vector3 pos2 = pos + n2 * d2;
 
-            int offset = GenTree(baseOffet + 8, r * 0.8f, new_n, new_pos, nbVertices);
+            r *= 0.8f;
+            alpha *= 1.0f;
+            beta *= 0.95f;
+  
 
-            circle = GenCircle(r, r*5, new_n, new_pos, nbVertices);
-            circle.CopyTo(vertices, nbVertices);
-            nbVertices += nbVertices;
+            nbVertices = GenCircle(vertices, uv, nbVertices, r, n1, pos1, vert, v);
+            nbTriangles = ConnectCircles(nbTriangles, baseOffet, baseOffet + vert, vert);
 
-            index = ConnectCircles(baseOffet, offset, nbVertices);
-            index.CopyTo(triangles, nbTriangles);
-            nbTriangles += index.Length;
+            int offset = GenTree(baseOffet + vert, r, l, alpha, beta, n1, pos1, vert, v + d1 * 0.5f);
 
-            return GenTree(offset, r * 0.8f, new_n, new_pos, nbVertices);
+            nbVertices = GenCircle(vertices, uv, nbVertices, r, n2, pos2, vert, v);
+            nbTriangles = ConnectCircles(nbTriangles, baseOffet, offset, vert);
+
+            return GenTree(offset, r, l, alpha, beta, n2, pos2, vert, v + d2 * 0.5f);
         }
         else
         {
-            Vector3 new_n = n;
-            Vector3 new_pos = pos;
+            Vector3 n_new = GenNormal(n, alpha, beta);
+            float d = l * Random.Range(0.8f, 1.2f);
+            Vector3 new_pos = pos + n_new * d;
 
-            Vector3[] circle = GenCircle(r, r*5, new_n, new_pos, nbVertices);
-            circle = GenCircle(r, r * 5, new_n, new_pos, nbVertices);
-            circle.CopyTo(vertices, nbVertices);
-            nbVertices += nbVertices;
+            r *= 0.9f;
+            alpha *= 1.0f;
+            beta *= 0.8f;
+ 
+            nbVertices = GenCircle(vertices, uv, nbVertices, r, n_new, new_pos, vert, v);
+            nbTriangles = ConnectCircles(nbTriangles, baseOffet, baseOffet + vert, vert);
 
-            int[] index = ConnectCircles(baseOffet, baseOffet + nbVertices, nbVertices);
-            index.CopyTo(triangles, nbTriangles);
-            nbTriangles += index.Length;
-
-            return GenTree(baseOffet, r * 0.8f, new_n, new_pos, nbVertices);
+            return GenTree(baseOffet + vert, r, l, alpha, beta, n_new, new_pos, vert, v + d * 0.5f);
         }
     }
 
-    void MakeMeshData() {
-        vertices = new Vector3[1000];
-        triangles = new int[1000];
-
-        Vector3[] circle1 = GenCircle(1, 4, new Vector3(0, 1, 0), new Vector3(0, 0, 0), 8);
-        Vector3[] circle2 = GenCircle(1, 4, new Vector3(0, 1, 0), new Vector3(0, 2, 0), 8);
-        Vector3[] circle3 = GenCircle(0.5f, 4, new Vector3(1, 1, 0), new Vector3(4, 4, 0), 8);
-        Vector3[] circle4 = GenCircle(0.5f, 4, new Vector3(-1, 1, 0), new Vector3(-4, 4, 0), 8);
-
-        circle1.CopyTo(vertices, 0);
-        circle2.CopyTo(vertices, circle1.Length);
-        circle3.CopyTo(vertices, circle1.Length + circle2.Length);
-        circle4.CopyTo(vertices, circle1.Length + circle2.Length + circle3.Length);
-
-        int[] t1 = ConnectCircles(0, 8, 8);
-        int[] t2 = ConnectCircles(8, 16, 8);
-        int[] t3 = ConnectCircles(8, 24, 8);
+    void MakeMeshData()
+    {
+        vertices = new Vector3[6000];
+        uv = new Vector2[6000];
+        triangles = new int[21000];
         
-        t1.CopyTo(triangles, 0);
-        t2.CopyTo(triangles, t1.Length);
-        t3.CopyTo(triangles, t1.Length + t2.Length);
+        nbVertices = 0;
+        nbTriangles = 0;
 
-        nbVertices = circle1.Length + circle2.Length + circle3.Length + circle4.Length;
-        nbTriangles = t1.Length + t2.Length + t3.Length;
+        nbVertices = GenCircle(vertices, uv, nbVertices, 3.0f, new Vector3(0, 1, 0), new Vector3(0, 0, 0), 8, 0.0f);
+        GenTree(0, 2.0f, 8f, 0.8f, 0.45f, new Vector3(0, 1, 0), new Vector3(0, 0, 0), 8, 4.0f);
     }
 
-	void CreateMesh() { 
+    void CreateMesh()
+    { 
 		mesh.Clear ();
+        Vector2[] mesh_uv = new Vector2[nbVertices];
         Vector3[] mesh_vertices = new Vector3[nbVertices];
 		int[] mesh_triangles = new int[nbTriangles];
 
@@ -235,10 +185,14 @@ public class proceduralTree : MonoBehaviour {
             mesh_triangles[i] = triangles[i];
 
         for (int i = 0; i < nbVertices; i++)
+        {
             mesh_vertices[i] = vertices[i];
+            mesh_uv[i] = uv[i];
+        }
 
         mesh.vertices = mesh_vertices;
         mesh.triangles = mesh_triangles;
+        mesh.uv = mesh_uv;
     }
 		
 }
